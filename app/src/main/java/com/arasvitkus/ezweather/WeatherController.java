b3af457931fcf59ed5d0b5f1e6e5890ee72b1202
arasmv <amvitkus@gmail.com>
2019-06-10 14:10:05 -0400

    Initial commit

---
+++ app/src/main/java/com/arasvitkus/ezweather/WeatherController.java
@@ -0,0 +1,13 @@
+package com.arasvitkus.ezweather;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WeatherController extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.weather_controller_layout);
+    }
+}
